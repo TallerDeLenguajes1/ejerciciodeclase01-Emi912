1444b1e93583815508fa8e18ab2a906956f7c17a
Emi Merchan <emimerchan83@gmail.com>
2021-08-18 17:50:42 -0300

    Problema inicial agregado

---
+++ ProblemaInicial/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ProblemaInicial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            int numAleatorio  = random.Next(0, 100);
+            Console.WriteLine("Escribe un numero");
+            int numElegido = Convert.ToInt32(Console.ReadLine());
+
+            do
+            { 
+                if (numElegido > numAleatorio)
+                {
+                    Console.WriteLine("El numero es mas chico");
+                    numElegido = Convert.ToInt32(Console.ReadLine());
+                }
+               else
+                {
+                    Console.WriteLine("El numero es mas grande");
+                    numElegido = Convert.ToInt32(Console.ReadLine());
+                }
+
+            } while (numElegido != numAleatorio);
+
+            if (numElegido == numAleatorio)
+            {
+                Console.WriteLine("El numero es correcto");
+            }
+        }
+    }
+}
